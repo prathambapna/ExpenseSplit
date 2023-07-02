61f159e3a9c514a145c6dfebf7228f72df435113
Pratham Bapna <manjubapna33@gmail.com>
2023-07-02 20:38:20 +0530

    logout and authentication added

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -45,10 +45,24 @@ exports.loginUser=catchAsyncErrors(async(req,res,next)=>{
 
 });
 
+//logout user
+exports.logoutUser=catchAsyncErrors(async(req,res,next)=>{
+    res.cookie("token",null,{
+        expires:new Date(Date.now()),
+        httpOnly:true,
+    });
+    res.status(200).json({
+        success:true,
+        message:"Logged out"
+    })
+});
+
 
 //get all users
-exports.getAllUsers=(req,res)=>{
+exports.getAllUsers=catchAsyncErrors(async(req,res)=>{
+    const users=await User.find();
     res.status(200).json({
-        message:"route working fine"
+        success:true,
+        users,
     })
-}
+});
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express=require("express");
-const { getAllUsers, registerUser, loginUser } = require("../controller/userController");
+const { getAllUsers, registerUser, loginUser, logoutUser } = require("../controller/userController");
+const isAuthenticatedUser = require("../middleware/auth");
 const router=express.Router();
 
-router.route("/users").get(getAllUsers);
+router.route("/users").get(isAuthenticatedUser, getAllUsers);
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/logout").get(logoutUser);
 
 
 module.exports=router;
