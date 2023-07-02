724d6f7b31d03f5d0936b94c042bac1ede8468f3
Pratham Bapna <manjubapna33@gmail.com>
2023-07-02 18:47:54 +0530

    login and register function added

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,3 +1,52 @@
+const ErrorHandler=require("../utils/errorHandler");
+const catchAsyncErrors=require("../middleware/catchAsyncError");
+const User=require("../models/userModel");
+const sendToken=require("../utils/jwtToken");
+
+
+
+//register a user
+exports.registerUser=catchAsyncErrors(async(req,res,next)=>{
+    const {name,email,password}=req.body;
+    const user =await User.create({
+        name,email,password,
+        avatar:{
+            public_id:"sample public id",
+            url:"profile pic url"
+        }
+    });
+
+    const token =user.getJWTToken();
+
+    sendToken(user,201,res);
+});
+
+//login user
+exports.loginUser=catchAsyncErrors(async(req,res,next)=>{
+    const {email,password}=req.body;
+    //checking if user has given email and password both
+    if(!email || !password){
+        return next(new ErrorHandler("Please enter email and password",400));
+    }
+    
+    const user=await User.findOne({email:email}).select("+password");
+
+    if(!user){
+        return next(new ErrorHandler("Invalid email or password"),401);
+    }
+
+    const isPasswordMatched=user.comparePassword(password);
+
+    if(!isPasswordMatched){
+        return next(new ErrorHandler("Invalid email or password"),401);
+    }
+
+    sendToken(user,200,res);
+
+});
+
+
+//get all users
 exports.getAllUsers=(req,res)=>{
     res.status(200).json({
         message:"route working fine"
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,10 @@
 const express=require("express");
-const { getAllUsers } = require("../controller/userController");
+const { getAllUsers, registerUser, loginUser } = require("../controller/userController");
 const router=express.Router();
 
 router.route("/users").get(getAllUsers);
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+
+
 module.exports=router;
