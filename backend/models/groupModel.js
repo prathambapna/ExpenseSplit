992da391ec07e4f642c52da34b0604be32f209ed
Pratham Bapna <manjubapna33@gmail.com>
2023-07-08 11:27:33 +0530

    added balance field in group and updated functions accordingly

--- backend/models/groupModel.js
+++ backend/models/groupModel.js
@@ -26,7 +26,31 @@ const groupSchema=new mongoose.Schema({
             type:mongoose.Schema.ObjectId,
             ref:"Expense"
         }
-    ]
+    ],
+    balances:[
+        {
+            userFrom: {
+                type: mongoose.Schema.ObjectId,
+                ref: "User",
+                required: true,
+            },
+            userTo: {
+                type: mongoose.Schema.ObjectId,
+                ref: "User",
+                required: true,
+            },
+            group: {
+                type: mongoose.Schema.ObjectId,
+                ref: "Group",
+                required: true,
+            },
+            balance: {
+                type: Number,
+                default: 0,
+            },    
+        }
+    ],
+    
 });
 
 module.exports=mongoose.model("Group",groupSchema);
