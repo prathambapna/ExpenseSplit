40a65b4f4330da1ae05d9d7b275c6dda1da37207
Pratham Bapna <manjubapna33@gmail.com>
2023-07-23 07:20:56 +0530

    frontend build

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cookieParser=require("cookie-parser");
 const errorMiddleware=require("./middleware/error");
 const bodyParser =require("body-parser");
 const fileUpload=require("express-fileupload");
+const path = require("path");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,6 +21,12 @@ app.use("/api/v1",user);
 app.use("/api/v1",groupRoute);
 app.use("/api/v1",expenseRoute);
 app.use("/api/v1",userBalanceRoute);
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 //middleware for error
 app.use(errorMiddleware);
--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -90,7 +90,7 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
   const resetToken = user.getResetPasswordToken();
   await user.save({ validateBeforeSave: false });
 
-  const resetPasswordUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
+  const resetPasswordUrl = `${req.protocol}://${req.get("host")}/password/reset/${resetToken}`;
   //eg : http://localhost/api/v1.....
 
   const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\n If you have not requested this email then please ignore it `;
